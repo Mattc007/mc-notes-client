d490aed23b5ff5c430c6d5eb6f316016219b7acd
Matt Catrett <mcatrett@turbosquid.com>
2018-05-06 14:17:30 -0500

    add nav links

---
+++ src/containers/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './Home.css'
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className='Home'>
+        <div className='lander'>
+          <h1>MC Notes</h1>
+          <p>A simple note taking app, because why not.</p>
+        </div>
+      </div>
+    )
+  }
+}
