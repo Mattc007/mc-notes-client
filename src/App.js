55209ec072a75a1d3a9f1ba8b75be62ac98adaf1
Matt Catrett <mcatrett@turbosquid.com>
2018-05-06 14:55:59 -0500

    functioning state + logout

--- src/App.js
+++ src/App.js
@@ -3,18 +3,35 @@ import Routes from './Routes'
 import { Link } from 'react-router-dom'
 import { Nav, Navbar, NavItem } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
+import { Auth } from 'aws-amplify'
 import './App.css'
 
 class App extends Component {
   state = {
     isAuthenticated: false,
+    isAuthenticating: true,
+  }
+
+  async componentDidMount() {
+    try {
+      if (await Auth.currentSession()) {
+        this.userHasAuthenticated(true)
+      }
+    } catch (e) {
+      if (e !== 'No current user') {
+        alert(e)
+      }
+    }
+
+    this.setState({ isAuthenticating: false })
   }
 
   userHasAuthenticated = authenticated => {
     this.setState({ isAuthenticated: authenticated })
   }
 
-  handleLogout = event => {
+  handleLogout = async event => {
+    await Auth.signOut()
     this.userHasAuthenticated(false)
   }
 
@@ -25,6 +42,7 @@ class App extends Component {
     }
 
     return (
+      !this.state.isAuthenticating &&
       <div className='App container'>
         <Navbar fluid collapseOnSelect>
           <Navbar.Header>
